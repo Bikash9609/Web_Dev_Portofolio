2905974a0a4fcb920c4ce1e39591882da122d3fe
bikashtiwari020 <bikashtiwari020@gmail.com>
2020-01-24 18:18:00 +0530

    Create script.js

---
+++ coffee-shop/script.js
@@ -0,0 +1,7 @@
+// import simpleParallax from "simpleParallax.min.js"
+
+var image = document.getElementsByClassName('bg-coffee');
+new simpleParallax(image, {
+	delay: .6,
+	transition: 'cubic-bezier(0,0,0,1)'
+});
